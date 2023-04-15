78e866cfaabb956fd1b92829d04cd63e088dfb5e
ProjetosUnama <128099809+ProjetosUnama@users.noreply.github.com>
2023-04-15 14:16:20 -0300

    Delete src directory

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-
-const PORT = process.env.PORT || 3001
-
-// ESCUTAR A PORTA A PORTA 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-})
